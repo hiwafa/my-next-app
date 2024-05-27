184b8619910aafde9c3837f981c1ee0be62a664c
hiwafa <mq.func@gmail.com>
2024-05-27 16:30:35 +0200

    use Routr worked

--- src/app/users/[user]/page.tsx
+++ src/app/users/[user]/page.tsx
@@ -1,12 +1,37 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
 export default ({params}: {
     params: {
         user: string
     }
 })=> {
 
+    const router = useRouter();
+
+    const backToHome = ()=> {
+        router.push('/');
+    }
+
+
+    const goBack = ()=> {
+        router.back();
+    }
+
+    const goForward = ()=> {
+        router.forward();
+    }
+
     return (
         <div>
             Username: {params.user}
+            <br />
+            <button onClick={backToHome}>Go to Home</button>
+            <br />
+            <button onClick={goBack}>Go Back</button>
+            <br />
+            <button onClick={goForward}>Go Forward</button>
         </div>
     )
 }
