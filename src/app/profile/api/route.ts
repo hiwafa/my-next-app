fdaa94922f83755ef80c2e620bd6659819806bcb
hiwafa <mq.func@gmail.com>
2024-05-31 16:27:29 +0200

    update route

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function Get() {
-    return new Response("Profile Api Data");
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    // Process a POST request
+
+    res.status(200).json({ message: 'Hello from Next.js!' })
+
+  } else {
+    // Handle any other HTTP method
+
+    res.status(200).json({ message: 'Hello from Next.js!!' })
+  }
 }
