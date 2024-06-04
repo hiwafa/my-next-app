c9271447d18c45a5d9a1099071921c52215be9f9
hiwafa <mq.func@gmail.com>
2024-06-04 15:44:56 +0200

    routing fixed using next.js

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,14 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
  
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    // Process a POST request
-
-    res.status(200).json({ message: 'Hello from Next.js!' })
-
-  } else {
-    // Handle any other HTTP method
-
-    res.status(200).json({ message: 'Hello from Next.js!!' })
-  }
+export async function GET() {
+  return new Response("The first api routing using next.js api");
 }
