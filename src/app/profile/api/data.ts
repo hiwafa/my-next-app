6be2d5570074f84e5bbeafea8a4b6fc743509d09
hiwafa <mq.func@gmail.com>
2024-06-04 15:59:21 +0200

    fake json data

---
+++ src/app/profile/api/data.ts
@@ -0,0 +1,22 @@
+export const profileDatas = [
+    {
+        id: 1,
+        username: "user1",
+    },
+    {
+        id: 2,
+        username: "user2",
+    },
+    {
+        id: 3,
+        username: "user3",
+    },
+    {
+        id: 4,
+        username: "user4",
+    },
+    {
+        id: 5,
+        username: "user1",
+    },
+]
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
- 
+import { profileDatas } from './data'; 
+
 export async function GET() {
-  return new Response("The first api routing using next.js api");
+  // return new Response("The first api routing using next.js api");
+  return Response.json(profileDatas);
 }
